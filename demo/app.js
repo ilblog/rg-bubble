dad184d8237383ade64d9c207db5a3d085255b2e
Greg <greg@flashtalking.com>
2015-07-08 14:59:20 +0100

    Updated Riot

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,4 @@
-riot.tag('demo-app', '<p> This is a <rg-bubble text="I go pop!"><em>bubble</em></rg-bubble> with some text in it! </p> <rg-bubble text="RiotGear FTW!"> <img src="img/logo.png"> </rg-bubble>', 'demo-app p { padding: 40px; line-height: 1.5; }', function (opts) {
+riot.tag('demo-app', '<p> This is a <rg-bubble text="I go pop!"><em>bubble</em></rg-bubble> with some text in it! </p> <rg-bubble text="RiotGear FTW!"> <img src="img/logo.png"> </rg-bubble>', 'demo-app p, [riot-tag="demo-app"] p{ padding: 40px; line-height: 1.5; }', function (opts) {
 
 
 });
